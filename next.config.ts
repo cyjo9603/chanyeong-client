aa481ce4937bb422047e1fa82d40509d4fc2e96e
조찬영 <cyjo9603@naver.com>
2024-12-20 22:32:37 +0900

    nextjs 추가 config 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
+const isDevelopment = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: "dist",
+  poweredByHeader: false,
+  output: "standalone",
+  rewrites: isDevelopment
+    ? async () => {
+        return [
+          {
+            source: "/graphql",
+            destination: "http://localhost:4011/graphql",
+          },
+        ].filter((v) => v);
+      }
+    : undefined,
 };
 
 export default nextConfig;
