2d164707216d95674faf0614b005feb2e49e6517
조찬영 <cyjo9603@naver.com>
2024-12-25 00:22:33 +0900

    메인 페이지 blog로 설정

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,35 @@
-import type { NextConfig } from "next";
-import path from "path";
+import type { NextConfig } from 'next';
+import path from 'path';
 
-const isDevelopment = process.env.NODE_ENV === "production";
+const isDevelopment = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  distDir: "dist",
+  distDir: 'dist',
   poweredByHeader: false,
-  output: "standalone",
+  output: 'standalone',
   sassOptions: {
-    includePaths: [path.join(__dirname, "src/")],
+    includePaths: [path.join(__dirname, 'src/')],
   },
   experimental: {
     turbo: {
       rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
         },
       },
     },
+  },
+  // TODO: 추후 다른 기능 생기면 제거
+  async redirects() {
+    return [{ source: '/', destination: '/blog', permanent: true }];
   },
   rewrites: isDevelopment
     ? async () => {
         return [
           {
-            source: "/graphql",
-            destination: "http://localhost:4011/graphql",
+            source: '/graphql',
+            destination: 'http://localhost:4011/graphql',
           },
         ].filter((v) => v);
       }
