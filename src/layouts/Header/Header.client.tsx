42f0ef8fae7397d5e6dc2ac537593d2b028f284a
조찬영 <cyjo9603@naver.com>
2025-01-27 20:26:49 +0900

    header hidden action 적용

--- src/layouts/Header/Header.client.tsx
+++ src/layouts/Header/Header.client.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import classNames from 'classnames/bind';
 import { useAtom } from 'jotai';
 import { useMutation, gql } from '@apollo/client';
+import { throttle } from 'lodash-es';
 
 import { Darkmode } from '@/constants/cookie.constant';
 import { LogoIcon, LogoTitleIcon, GithubIcon, DarkmodeIcon, LightmodeIcon } from '@/assets';
@@ -24,12 +25,20 @@ interface HeaderProps {
   darkmodeCookie: Darkmode;
 }
 
+const HEADER_HEIGHT = 48;
+
+const HEADER_HIDDEN_ACTION_PATHS = ['/blog/posts'];
+
 const Header: React.FC<HeaderProps> = ({ darkmodeCookie }) => {
   const pathname = usePathname();
   const [isDarkmode, changeDarkmode] = useDarkmode({ darkmodeCookie });
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const [isVisibleHeader, setIsVisibleHeader] = useState(true);
+
   const [user, setUser] = useAtom(userAtom);
+
+  const headerRef = useRef<HTMLHeadElement>(null);
 
   const [logoutMutation] = useMutation<LogoutMutation>(localMutation, {
     onCompleted: ({ logout }) => {
@@ -52,8 +61,43 @@ const Header: React.FC<HeaderProps> = ({ darkmodeCookie }) => {
     logoutMutation();
   };
 
+  useEffect(() => {
+    if (HEADER_HIDDEN_ACTION_PATHS.every((path) => !pathname.startsWith(path))) {
+      return;
+    }
+    const clickEventHandler = (e: MouseEvent) => {
+      if (headerRef.current?.contains(e.target as Node)) {
+        return;
+      }
+
+      const scrollTop = document.documentElement.scrollTop;
+
+      setIsVisibleHeader((prev) => {
+        if (scrollTop < HEADER_HEIGHT && prev) {
+          return prev;
+        }
+        return !prev;
+      });
+    };
+    const scrollHandler = throttle(() => {
+      const scrollTop = document.documentElement.scrollTop;
+
+      if (scrollTop < HEADER_HEIGHT) {
+        setIsVisibleHeader(true);
+      }
+    }, 200);
+
+    document.addEventListener('click', clickEventHandler, { capture: true });
+    document.addEventListener('scroll', scrollHandler);
+
+    return () => {
+      document.removeEventListener('click', clickEventHandler, { capture: true });
+      document.removeEventListener('scroll', scrollHandler);
+    };
+  }, [pathname]);
+
   return (
-    <header className={cx('Header', isDarkmode && 'bottom-line')}>
+    <header className={cx('Header', isDarkmode && 'bottom-line', !isVisibleHeader && 'visible')} ref={headerRef}>
       <div className={cx('inner')}>
         <div className={cx('main')}>
           <Link href="/" className={cx('vertical-center', 'icon')}>
@@ -79,12 +123,18 @@ const Header: React.FC<HeaderProps> = ({ darkmodeCookie }) => {
             </button>
           )}
           <div className={cx('division')} />
-          <a href={process.env.NEXT_PUBLIC_CY_GITHUB_URL} className={cx('vertical-center', 'icon')}>
+          <a
+            href={process.env.NEXT_PUBLIC_CY_GITHUB_URL}
+            className={cx('vertical-center', 'icon')}
+            target="_blank"
+            rel="noreferrer"
+          >
             <GithubIcon />
           </a>
           <div className={cx('division')} />
           <button onClick={changeDarkmode} className={cx('vertical-center', 'darkmode', 'icon')}>
             {isDarkmode ? <LightmodeIcon /> : <DarkmodeIcon />}
+            test
           </button>
         </div>
       </div>
